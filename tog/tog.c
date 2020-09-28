a9c6be9c3a5b8be1249b1848a2edefb2fdb64b02
Ed Maste <emaste@FreeBSD.org>
2020-09-28 11:14:30 -0400

    [upstream] use size_t for loop indices to avoid signedness warnings
    Same change as 16aeacf7088d, for subdirectories other than lib/

--- tog/tog.c
+++ tog/tog.c
@@ -6353,7 +6353,7 @@ done:
 static void
 list_commands(FILE *fp)
 {
-	int i;
+	size_t i;
 
 	fprintf(fp, "commands:");
 	for (i = 0; i < nitems(tog_commands); i++) {
@@ -6541,7 +6541,7 @@ main(int argc, char *argv[])
 		argc = 1;
 		cmd_argv = make_argv(argc, cmd->name);
 	} else {
-		int i;
+		size_t i;
 
 		/* Did the user specify a command? */
 		for (i = 0; i < nitems(tog_commands); i++) {
